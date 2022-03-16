defcfcd57b2b140a15a6d03a9d101e3019f9bdc1
moshdealer <mikhailkonchakov@gmail.com>
2022-03-16 19:44:09 +0300

    выполнил третью лабу

--- lab3/2.js
+++ lab3/2.js
@@ -7,8 +7,18 @@
  * Примеры:
  * '4 и -6, 2, 1, может 9, 63, -134 и 566]' -> {min: -134, max: 566}
  */
-function getMinMax(str) {
-    // code here
+ function getMinMax(str) {
+    let ar = str.split(/[\]\ !?;,]/);
+    let min = Number(ar[0]);
+    let max = Number (ar[0]);
+    for (let i = 1; i < ar.length; i++){
+        if ((ar[i] > max)&&(!isNaN(ar[i]))){
+            max = Number(ar[i]);
+        }
+        if ((ar[i] < min)&&(!isNaN(ar[i]))){
+            min = Number(ar[i]);
+        }
+    }
+  return {min, max};
 }
-
 module.exports = getMinMax;
--- lab3/4.js
+++ lab3/4.js
@@ -8,7 +8,12 @@
  */
 
 function prettySum(arr) {
-    // code here
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (i % 2 == 0){
+            sum += arr[i] * arr[arr.length-1];
+        }
+    }
+    return (sum);
 }
-
 module.exports = prettySum;
